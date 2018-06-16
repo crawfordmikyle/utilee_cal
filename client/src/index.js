f4861cee3e6243c46d69c10c8bcd0eb3a06ecf35
Mikyle A Crawford <crawfordmikyle@gmail.com>
2018-06-16 14:59:20 -0700

    installed and messed around with react and matrial UI

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,12 @@
+//React Imports
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+//Redux Imports
+
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
